efdb3981b2014f43db88e120dd8089c69bfd8991
jinshah0322 <jinshah0322@gmail.com>
2022-10-31 10:36:31 +0530

    written query to fetch data

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,12 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from accounts.models import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+    total_orders = orders.count()
+    total_delivered = orders.filter(status='Delivered').count()
+    total_pending = orders.filter(status="Pending").count()
+    context = {'orders':orders,'customers':customers,'total_orders':total_orders,'total_delivered':total_delivered,'total_pending':total_pending}
+    return render(request, 'accounts/dashboard.html',context)
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html',{'products':products})
 
 def customer(request):
     return render(request, 'accounts/customer.html')
