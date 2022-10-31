48f051f97d6f6df0b19e7fdb432bf5961284c6d6
jinshah0322 <jinshah0322@gmail.com>
2022-10-31 13:51:28 +0530

    added path form crud operations on orders

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='products'),
-    path('customer/',views.customer, name='customer'),
+    path('customer/<str:pk>/',views.customer, name='customer'),
+    path('create_order/',views.createOrders, name='create_order'),
+    path('update_order/<str:pk>/',views.updateOrder ,name="update_order"),
+    path('delete_order/<str:pk>/',views.deleteOrder ,name="delete_order"),
 ]
